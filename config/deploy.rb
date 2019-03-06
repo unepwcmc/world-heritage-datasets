2d5b0165408404a9bc222f3f92d95690df8e02c3
Stuart Watson <stuart.watson@unep-wcmc.org>
2019-03-06 12:42:12 +0000

    Added dataset files

---
+++ config/deploy.rb
@@ -0,0 +1,50 @@
+lock '3.4.0'
+
+set :application, 'world-heritage-datasets'
+set :repo_url, 'git@github.com:unepwcmc/world-heritage-datasets.git'
+
+set :branch, 'master'
+
+set :deploy_user, 'wcmc'
+set :deploy_to, "/home/#{fetch(:deploy_user)}/#{fetch(:application)}"
+
+set :pty, true
+
+
+set :ssh_options, {
+  forward_agent: true,
+}
+
+
+set :keep_releases, 5
+
+set :passenger_restart_with_touch, false
+
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml"
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for local_user is ENV['USER']
+# set :local_user, -> { `git config user.name`.chomp }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+# Uncomment the following to require manually verifying the host key before first deploy.
+# set :ssh_options, verify_host_key: :secure
